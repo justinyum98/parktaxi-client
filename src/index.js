acc8d86b899cb0160198b5062df83c667b549636
Justin Yum <justinyum98@gmail.com>
2020-02-13 15:34:23 -0800

    added prettier plus airbnb eslint config, removed redux from project

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
 import App from './App';
-import rootReducer from './redux/reducers';
 import * as serviceWorker from './serviceWorker';
 import './style.css';
 
-const middleWare = [thunk];
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  {},
-  composeEnhancers(applyMiddleware(...middleWare)),
-);
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/redux/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const CHANGE_MODE = "CHANGE_MODE";
-export const TOGGLE_LOT = "TOGGLE_LOT";
-export const SELL_SPOT = "SELL_POST";
-export const GET_SPOTS = "GET_SPOTS";
-export const LOADING_SPOTS = "LOADING_SPOTS";
